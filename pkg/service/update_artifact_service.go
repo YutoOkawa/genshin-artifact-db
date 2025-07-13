beb61b39aaf5d787699db53e6ddfcc1c0c7b53cb
YutoOkawa <yuutoowata.0305@gmail.com>
2025-07-13 03:27:19 +0000

    Add CreateArtifact service

---
+++ pkg/service/update_artifact_service.go
@@ -0,0 +1,27 @@
+package service
+
+import (
+	"genshin-artifact-db/pkg/entity"
+	"genshin-artifact-db/pkg/repository"
+)
+
+type UpdateArtifactServiceInterface interface {
+	CreateArtifact(artifact entity.Artifact) error
+}
+
+type UpdateArtifactService struct {
+	artifactSaver repository.ArtifactSaver
+}
+
+func NewUpdateArtifactService(artifactSaver repository.ArtifactSaver) *UpdateArtifactService {
+	return &UpdateArtifactService{
+		artifactSaver: artifactSaver,
+	}
+}
+
+func (s *UpdateArtifactService) CreateArtifact(artifact entity.Artifact) error {
+	if err := s.artifactSaver.SaveArtifact(&artifact); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ pkg/service/update_artifact_service_test.go
@@ -0,0 +1,59 @@
+package service
+
+import (
+	"errors"
+	"genshin-artifact-db/pkg/entity"
+	"genshin-artifact-db/pkg/repository"
+	"testing"
+)
+
+func TestUpdateArtifactServiceCreateArtifact(t *testing.T) {
+	testArtifact := entity.Artifact{
+		ID: "test-id",
+	}
+
+	tests := []struct {
+		name string
+
+		// GIVEN
+		mockArtifactSaverError error
+
+		// WHEN
+		artifact entity.Artifact
+
+		// THEN
+		expectedError bool
+	}{
+		{
+			name: "ShouldCreateArtifactSuccessfully",
+
+			artifact: testArtifact,
+
+			expectedError: false,
+		},
+		{
+			name: "ShouldReturnErrorWhenArtifactSaverFails",
+
+			mockArtifactSaverError: errors.New("artifact saver error"),
+
+			expectedError: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockArtifactSaver := &repository.MockArtifactSaver{
+				SaveArtifactError: tt.mockArtifactSaverError,
+			}
+
+			service := UpdateArtifactService{
+				artifactSaver: mockArtifactSaver,
+			}
+
+			err := service.CreateArtifact(tt.artifact)
+			if (err != nil) != tt.expectedError {
+				t.Errorf("CreateArtifact() error = %v, expectedError %v", err, tt.expectedError)
+			}
+		})
+	}
+}
